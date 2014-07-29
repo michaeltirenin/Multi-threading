05ede5afc1935fde5e1ee9647950177af6b09516
Michael Tirenin <michaeltirenin@gmail.com>
2014-07-28 19:29:11 -0700

    initial commit

--- MultiThreading/ViewController.swift
+++ MultiThreading/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imageView.layer.borderWidth = 1.0
+        imageView.layer.borderColor = UIColor.grayColor().CGColor
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func downloadButton(sender: UIButton) {
+        
+        var url = NSURL(string: "http://upload.wikimedia.org/wikipedia/commons/e/eb/Nancy_Reagan_reopens_Statue_of_Liberty_1986.jpg")
+        
+        var myQueue = NSOperationQueue()
+        
+        myQueue.addOperationWithBlock({() -> Void in
+            
+            var data = NSData(contentsOfURL: url)
+            NSOperationQueue.mainQueue().addOperationWithBlock( {() -> Void in
+            
+                    var myImage = UIImage(data: data)
+                
+                    self.imageView.image = myImage
+                })
+            
+            })
+    }
 }
 
